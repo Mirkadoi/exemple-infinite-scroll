9af3d094171953f5afb4f64ed5c778e657a80de7
Мухутдинов Искандер <mi@smarthead.ru>
2020-03-29 16:56:37 +0300

    Добавляет экранирование логина

--- src/components/Calculator/CalcInfoUser.js
+++ src/components/Calculator/CalcInfoUser.js
@@ -5,9 +5,23 @@ import styles from './CalcInfoUser.module.scss';
 
 const CalcInfoUser = ({ stepCalc }) => {
     const login = localStorage.getItem('login');
+    const matchResult = login.match(/(?<=@.).*(?=\.)/);
+    const [partDomainName] = matchResult;
+
+    const writeStars = () => {
+        let str = '';
+        for (let i = 0; i < partDomainName.length; i += 1) {
+            str += '*';
+        }
+
+        return str;
+    };
+
+    const prependLogin = login.replace(`${partDomainName}`, writeStars);
+
     return (
         <div className={styles.info}>
-            <p>Привет, {login}!</p>
+            <p>Привет, {prependLogin}!</p>
             <p>Этап: "{stepCalc}"</p>
         </div>
     );
